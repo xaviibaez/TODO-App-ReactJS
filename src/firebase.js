10a9d0090044a5c2f334a2123f36f4f367c8507d
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-10-16 00:31:32 +0200

    Create firebase.js
    1.30

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA40FPYrZChsWGAjLdKh9f3WEERauMadD0",
+    authDomain: "todo-app-cp-2d17d.firebaseapp.com",
+    databaseURL: "https://todo-app-cp-2d17d.firebaseio.com",
+    projectId: "todo-app-cp-2d17d",
+    storageBucket: "todo-app-cp-2d17d.appspot.com",
+    messagingSenderId: "105164647078",
+    appId: "1:105164647078:web:523cd524e5edea7f412267",
+    measurementId: "G-50TVLCKG3R"
+  };
