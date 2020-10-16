bd29e7fdfab2c18c394c8836afe9464e00c1a9d9
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-10-16 12:00:58 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+/*
 const firebaseConfig = {
     apiKey: "AIzaSyA40FPYrZChsWGAjLdKh9f3WEERauMadD0",
     authDomain: "todo-app-cp-2d17d.firebaseapp.com",
@@ -8,3 +9,24 @@ const firebaseConfig = {
     appId: "1:105164647078:web:523cd524e5edea7f412267",
     measurementId: "G-50TVLCKG3R"
   };
+  */
+
+import firebase from "firebase";
+
+//Constantes que sacamos de la configuracion del proyecto en firebase
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyA40FPYrZChsWGAjLdKh9f3WEERauMadD0",
+  authDomain: "todo-app-cp-2d17d.firebaseapp.com",
+  databaseURL: "https://todo-app-cp-2d17d.firebaseio.com",
+  projectId: "todo-app-cp-2d17d",
+  storageBucket: "todo-app-cp-2d17d.appspot.com",
+  messagingSenderId: "105164647078",
+  appId: "1:105164647078:web:523cd524e5edea7f412267",
+  measurementId: "G-50TVLCKG3R"
+});
+
+//Con esta constante conectamos a la bd de firebase y lo guardamos todo en el objeto db
+const db = firebase.firestore();
+
+//Lo exportamos asi tenemos acceso a db des de cualquier lugar de la app
+export { db };
