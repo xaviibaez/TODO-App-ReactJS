52d8118ce449ef216ef96a99e91464d13a903fef
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-10-18 20:43:58 +0200

    style modal
    style modal

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,27 @@
-import { ListItem, ListItemAvatar, ListItemText, Modal } from '@material-ui/core'
+import { ListItem, ListItemAvatar, ListItemText, Modal, Button } from '@material-ui/core';
+import { List } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import React , { useState } from 'react';
-import './Todo.css'
-import { List } from '@material-ui/core';
-import db from './firebase'
+import './Todo.css';
+import db from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+
+//Style para el modal
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
 //Componente Todo
 //Props -> propiedades - Lo que nos viene de App.js en forma de parametro
 function Todo(props) {
+    const classes = useStyles();
     const [open, setOpen] = useState(false);
 
     const handleOpen = () => {
@@ -19,9 +33,10 @@ function Todo(props) {
         {/* React fragment */}
         {/* Modal edit todo, se abrira cuando la variable open = true */}
         <Modal open={open} onClose={e => setOpen(false)}>
-            <div>
+            {/* Importamos el style del modal */}
+            <div className={classes.paper}>
                 <h1>Modal</h1>
-                <button onClick={e => setOpen(false)}></button>
+                <Button onClick={e => setOpen(false)}>Edit</Button>
             </div>
         </Modal>
         <List className="todo__list">
